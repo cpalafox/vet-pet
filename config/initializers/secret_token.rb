af9d2651664c7cd9b0cad03a708bf1eee77bea96
Cesar Palafox <cpaguila1@gmail.com>
2014-03-04 00:30:59 -0600

    Creates the new vet-pet project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VetPet::Application.config.secret_key_base = 'c13cce5067cdf6ff55f2da9806ad95e43d1ffb9f95744911b9ae1c39f95c1deb7c3c897dc02bb23afedcc7f81e4f6eeaba0f5ad366f989f3970620539a62db8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VetPet::Application.config.session_store :cookie_store, key: '_vet-pet_session'
